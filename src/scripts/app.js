3a05dbfa81d51ce0e2a0d0afd182164d9ae30912
jwebmaster <justin@jwebmaster.be>
2023-04-26 09:49:53 +0200

    Avec url images et id dans le json

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,71 @@
 console.info('Hello world');
+
+import Chart from 'chart.js/auto';
+
+
+const numberOfObjectIn = document.getElementById('numberOfObjectIn');
+
+fetch('/assets/datas/dataset.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    console.log(json);
+    //traitement
+
+    var datas = json;  
+
+  
+    var data = [
+      { position: "Doigt", count: 0 },
+      { position: "Visage", count: 2 },
+      { position: "Pieds", count: 7 },
+      { position: "Mains", count: 4 },
+      { position: "Nez", count: 4 },
+      { position: "Doigt", count: 25 },
+      { position: "Visage", count: 2 },
+      { position: "Pieds", count: 7 },
+      { position: "Mains", count: 4 },
+      { position: "Nez", count: 4 },
+
+    ]
+    new Chart(
+      document.getElementById('numberOfObjectIn'),
+      {
+        type: 'radar',
+        options: {
+          animation: true,
+          responsive: true,
+          plugins: {
+            legend: {
+              display: true
+            },
+            tooltip: {
+              enabled: true
+            }
+          }
+        },
+        data: {
+          labels: data.map(row => row.position),
+          datasets: [
+            {
+              label: "Nombre de fois que " + "Machines à coudre" + " a été trouvé",
+              data: data.map(row => row.count)
+            }
+          ]
+        }
+      }
+    );
+
+    // var container = document.querySelector('.content');
+    // var content = "<h2>" + datas.name + "</h2>" + "<p>" + datas.sections[0] + "</p>";
+    // container.innerHTML = content;
+  })
+  .catch((error) => {
+    console.log('Error: (' + error +')');
+  });
+
+
+  
+
+  
