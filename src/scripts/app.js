1cc8fdb4eb820fcba4fdb31ff4562ba0703470f4
jwebmaster <justin@jwebmaster.be>
2023-05-02 16:14:20 +0200

    version ok avec toutes les partie du corps

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,112 +1,101 @@
 import Chart from 'chart.js/auto';
 
-var graph = new Chart(
-  document.getElementById('numberOfObjectIn'),
-  {
-    type: 'radar',
-    options: {
-      animation: true,
-      responsive: true,
-      plugins: {
-        legend: {
-          display: true
-        },
-        tooltip: {
-          enabled: true
-        }
-      }
-    }
-  });
+var positionName = ["Épaule", "Tronc supérieur", "Coude", "Avant bras", "Poignet", "Genoux", "Partie inférieure de la jambe", "Cheville", "Parties intimes", "Tête", "Visages", "Globe occulaire", "Tronc inférieur", "Partie supérieure du bras", "Partie supérieure de la jambe", "Main", "Pied", "Tout le corps", "Inconnus", "Bouche", "Cou", "Doigt", "Orteille", "Oreille"];
+
+var Highcharts = require('highcharts');  
+// Load module after Highcharts is loaded
+require('highcharts/modules/exporting')(Highcharts);  
+// Create the chart
+Highcharts.chart('container', { /*Highcharts options*/ });
+
 
 const numberOfObjectIn = document.getElementById('numberOfObjectIn');  
 
 var buttonsObject = document.querySelectorAll('.buttonsObject');
 
+
 buttonsObject.forEach(button => {
   button.addEventListener('click', function() { 
     
-    fetch('/assets/datas/dataset.json')
-      .then((response) => {
-        return response.json();
-      })
-      .then((json) => {
-        // console.log(json);
-        //traitement
+  fetch('/assets/datas/dataset.json')
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      // console.log(json);
+      //traitement
 
-        var datas = json; 
+      var datas = json; 
 
-        var idObject = this.getAttribute('data-id');
-        console.log(idObject);
-    
-        // fait la recherche de l'objet xx dans le json
-        var item = datas.find(el => el.id == idObject);
-        console.log(item);
+      var idObject = this.getAttribute('data-id');
+      console.log(idObject);
+  
+      // fait la recherche de l'objet xx dans le json
+      var item = datas.find(el => el.id == idObject);
+      console.log(item);
 
-        var objet = item.objet;
-        var positions = item.position;
-        console.log(objet);
-        console.log(positions);
+      var objet = item.objet;
+      var positions = item.position;
+      console.log(objet);
+      console.log(positions);
 
-        var positionTable = [];
+      var positionTable = [];
+      var positionNameTable = [];
+      console.log(positionTable);
 
-        for (const key in positions) {
-          // if (Object.hasOwnProperty.call(positions, key)) {
-          //   const element = positions[key];
-          //   console.log(element)
-          //   console.log(key);
-          // }
-          positionTable.push([key, positions[key]]);
-        }
-        console.log(positionTable);
+      for (const key in positions) {
+        // if (Object.hasOwnProperty.call(positions, key)) {
+        //   const element = positions[key];
+        //   console.log(element)
+        //   console.log(key);
+        // }
+        positionTable.push(positions[key]);
+      }
+      for (const key in positions) {
+        // if (Object.hasOwnProperty.call(positions, key)) {
+        //   const element = positions[key];
+        //   console.log(element)
+        //   console.log(key);
+        // }
+        positionNameTable.push(key);
+      }
+      console.log(positionTable);
+      console.log(positionNameTable);
 
-        
-        // Nombre de fois que l'objet a été trouvé dans ...
-        var countArray = [];
-        positionTable.forEach(element => countArray.push(element[1]),
-        console.log(countArray));
-        
-        var positionArray = [];
-        positionTable.forEach(element => positionArray.push(element[0]),
-        console.log(positionArray));
+      positionNameTable.forEach(element => {
+        var i = 0;
+        console.log(element);
+        positionNameTable[i] = positionName[i];
+        console.log(positionNameTable[i]);
+        i++;
+      });
+
+      const chart = Highcharts.chart(container, {
+        chart: {
+          type: 'column'
+        },
+        title: {
+          text: "Nombre de fois que " + objet + " a été trouvé dans..."
+        },
+        xAxis: {
+          categories: positionName // Utiliser les catégories récupérées depuis le fichier JSON
+        },
+        yAxis: {
+          title: {
+            text: "Quantité"
+          }
+        },
+        series: [{
+          name: "Quantité de " + objet + " trouvés",
+          data: positionTable // Utiliser les données récupérées depuis le fichier JSON
+        },
+      ]
+      });
 
 
-
-        graph.destroy();
-        graph = new Chart(
-          document.getElementById('numberOfObjectIn'),
-          {
-            type: 'bar',
-            options: {
-              animation: true,
-              responsive: true,
-              plugins: {
-                legend: {
-                  display: true
-                },
-                tooltip: {
-                  enabled: true
-                }
-              }
-            },
-            data: {
-              labels: positionArray,
-              datasets: [
-                {
-                  label: "Nombre de fois que " + objet + " a été trouvé",
-                  data: countArray
-                }
-              ]
-            }
-          });
-          graph;
-        })
-        .catch((error) => {
-          console.log('Error: (' + error +')');
-        });
-      })
-      
+    })
+    .catch((error) => {
+        console.log('Error: (' + error +')');
     });
-
-  
-
-  
+  });
+});
