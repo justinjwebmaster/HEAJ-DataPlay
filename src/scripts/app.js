6b262a3f477171d6b41d0e9ef09554e3fa49430c
jwebmaster <justin@jwebmaster.be>
2023-05-08 14:35:34 +0200

    Script pour modification filAriane

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
-import Chart from 'chart.js/auto';
+'use strict';
 
 var position = ["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10"];
 
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,41 @@
 'use strict';
 
-console.log('Hello, world!');
+var position = ["p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10"];
+
+var positionName = ["Bras", "Main", "Tête", "Tronc supérieur", "Parties intimes", "Jambes", "Pieds", "Oreilles", "Bouches", "Oeil"];
+
+var objectName = ["Couvert", "Cure-dents", "Bougies", "Matériel d'art", "Statues et vases", "Batteries", "Savons", "Bouteilles", "Sextoys", "Bijoux", "Fournitures de bureau", "Clous - Vis"];
+
+// déclaration variables
+var filAriane = document.querySelector('.filAriane');
+
+
+// modification du headerLeft (/partie du corps) par la partie du corps sélectionnée
+var buttonPart = document.querySelectorAll('.buttonsBulles');
+
+buttonPart.forEach(function(button) {
+    button.addEventListener('click', changeArianePart);
+});
+
+function changeArianePart() {
+  var idPart = this.getAttribute('data-id');
+  console.log(idPart);
+  var partSelected = positionName[idPart-1];
+
+  filAriane.textContent = partSelected;
+}
+
+// modification du headerLeft (/objet) par l'objet sélectionné
+var buttonPart = document.querySelectorAll('.buttonsObject');
+
+buttonPart.forEach(function(button) {
+    button.addEventListener('click', changeArianeObject);
+});
+
+function changeArianeObject() {
+  var idObject = this.getAttribute('data-id');
+  console.log(idObject);
+  var objectSelected = objectName[idObject-1];
+
+  filAriane.textContent = objectSelected;
+}
